a266b0b85aafaea8d8a3cde9da1909f3760b8555
TravelFiend <mikegrace274@gmail.com>
2019-10-03 12:48:03 -0700

    I'm still working out the reset button and throws images, but this is as far as I could get for now

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const drawSpan = document.getElementById('draw');
 const playButton = document.getElementById('play-button');
 const resetButton = document.getElementById('reset-button');
 const outcomeMessage = document.getElementById('outcome-message');
+const twoThrows = document.getElementById('twoThrows');
 
 const roc = document.createElement('img');
 roc.className = "smallPics";
@@ -23,9 +24,10 @@ let winCount = 0;
 let loseCount = 0;
 let drawCount = 0;
 
+let randoThrow = getRandomThrow();
+
 // set a function that shows an image of which throw was thrown by comp
 const showCompThrow = (randoThrow) => {
-    randoThrow = getRandomThrow();
     if (randoThrow === 'rock'){
         computerThrow.appendChild(roc);
     } else if (randoThrow === 'paper'){
@@ -37,9 +39,10 @@ const showCompThrow = (randoThrow) => {
 
 // set a function that shows an image of which throw was thrown by user
 const showUserThrow = () => {
-    if (getRandomThrow() === 'rock') {
+    const userChoice = document.querySelector('input:checked').value;
+    if (userChoice === 'rock') {
         userThrow.appendChild(roc);
-    } else if (getRandomThrow() === 'paper') {
+    } else if (userChoice === 'paper') {
         userThrow.appendChild(pape);
     } else {
         userThrow.appendChild(sciss);
@@ -60,15 +63,13 @@ const hardReset = () => {
     winCount = 0;
     loseCount = 0;
     drawCount = 0;
-    var images = document.getElementById('smallPics');
-    userThrow.removeChild('images');
-    computerThrow.removeChild('images');
+    const images = document.getElementsByClassName('smallPics');
+    twoThrows.removeChild(images);
     updateSpans();
 };
 
 // set a function for playing a round
 const playARound = () => {
-
     const userChoice = document.querySelector('input:checked').value;
     const computerChoice = getRandomThrow();
     const attempt = checkResult(userChoice, computerChoice);
@@ -80,6 +81,7 @@ const playARound = () => {
     } else if (attempt === 'draw') {
         drawCount++;
     }
+    outcomeMessage.textContent = attempt;
     updateSpans();
 }
 
