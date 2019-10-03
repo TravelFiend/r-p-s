6a4466cb2e156f86cf2e40a115db239382a9bf6d
TravelFiend <mikegrace274@gmail.com>
2019-10-03 00:21:01 -0700

    Added CSS to match wireframe

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 import checkResult, { getRandomThrow } from './check-checkResult.js';
 
+const computerThrow = document.getElementById('computer-throw');
+const userThrow = document.getElementById('user-throw');
+const wins = document.getElementById('win');
+const loses = document.getElementById('lose');
+const draws = document.getElementById('draw');
+const playButton = document.getElementById('draw');
+const resetButton = document.getElementById('draw');
