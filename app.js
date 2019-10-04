dc5723bf53280fd727c5e69c8d133e81bf7dbb92
TravelFiend <mikegrace274@gmail.com>
2019-10-03 22:00:09 -0700

    Fixed throw image functionality

--- app.js
+++ app.js
@@ -1,61 +1,83 @@
 import checkResult, { getRandomThrow } from './get-random-throw.js';
 
-const computerThrow = document.getElementById('computer-throw');
-const userThrow = document.getElementById('user-throw');
 const winSpan = document.getElementById('win');
 const loseSpan = document.getElementById('lose');
 const drawSpan = document.getElementById('draw');
 const playButton = document.getElementById('play-button');
 const resetButton = document.getElementById('reset-button');
 const outcomeMessage = document.getElementById('outcome-message');
-const twoThrows = document.getElementById('twoThrows');
+const computerThrow = document.getElementsByClassName('computer-throw');
+const userThrow = document.getElementsByClassName('user-throw');
+const computerRockPic = document.getElementById('computer-rock-pic');
+const computerPaperPic = document.getElementById('computer-paper-pic');
+const computerScissorsPic = document.getElementById('computer-scissors-pic');
+const userRockPic = document.getElementById('user-rock-pic');
+const userPaperPic = document.getElementById('user-paper-pic');
+const userScissorsPic = document.getElementById('user-scissors-pic');
 
 const roc = document.createElement('img');
-roc.className = "smallPics";
+roc.className = 'smallPics';
 roc.src = './assets/rock.jpg';
 const pape = document.createElement('img');
-pape.className = "smallPics";
+pape.className = 'smallPics';
 pape.src = './assets/paper.jpg';
 const sciss = document.createElement('img');
-sciss.className = "smallPics";
+sciss.className = 'smallPics';
 sciss.src = './assets/scissors.jpg';
 
+winSpan.textContent = 0;
+loseSpan.textContent = 0;
+drawSpan.textContent = 0;
 let winCount = 0;
 let loseCount = 0;
 let drawCount = 0;
 
-let randoThrow = getRandomThrow();
-
 // set a function that shows an image of which throw was thrown by comp
-const showCompThrow = (randoThrow) => {
-    if (randoThrow === 'rock'){
-        computerThrow.appendChild(roc);
-    } else if (randoThrow === 'paper'){
-        computerThrow.appendChild(pape);
+const showComputerThrowImg = (compPic) => {
+    if (compPic === 'rock') {
+        computerRockPic.classList.remove('hidden');
+    } else if (compPic === 'paper') {
+        computerPaperPic.classList.remove('hidden');
     } else {
-        computerThrow.appendChild(sciss);
+        computerScissorsPic.classList.remove('hidden');
     }
 };
+
+const hideComputerThrowImage = () => {
+    computerRockPic.classList.add('hidden');
+    computerPaperPic.classList.add('hidden');
+    computerScissorsPic.classList.add('hidden');
+}
 
 // set a function that shows an image of which throw was thrown by user
-const showUserThrow = () => {
-    const userChoice = document.querySelector('input:checked').value;
-    if (userChoice === 'rock') {
-        userThrow.appendChild(roc);
-    } else if (userChoice === 'paper') {
-        userThrow.appendChild(pape);
+const showUserThrowImg = (userPic) => {
+    if (userPic === 'rock') {
+        userRockPic.classList.remove('hidden');
+    } else if (userPic === 'paper') {
+        userPaperPic.classList.remove('hidden');
+    } else if (userPic === 'scissors') {
+        userScissorsPic.classList.remove('hidden');
     } else {
-        userThrow.appendChild(sciss);
+        return;
     }
 };
 
+const hideUserThrowImage = () => {
+    userRockPic.classList.add('hidden');
+    userPaperPic.classList.add('hidden');
+    userScissorsPic.classList.add('hidden');
+}
 
+// set a function for updating spans
 const updateSpans = () => {
-    showCompThrow();
-    showUserThrow();
     winSpan.textContent = winCount;
     loseSpan.textContent = loseCount;
     drawSpan.textContent = drawCount;
+};
+
+const updateImages = (userThrow, computerThrow) => {
+    showComputerThrowImg(computerThrow);
+    showUserThrowImg(userThrow);
 };
 
 // set a function for reset button
@@ -63,13 +85,16 @@ const hardReset = () => {
     winCount = 0;
     loseCount = 0;
     drawCount = 0;
-    const images = document.getElementsByClassName('smallPics');
-    twoThrows.removeChild(images);
+    outcomeMessage.textContent = '';
     updateSpans();
+    hideComputerThrowImage();
+    hideUserThrowImage();
 };
 
 // set a function for playing a round
 const playARound = () => {
+    hideComputerThrowImage();
+    hideUserThrowImage();
     const userChoice = document.querySelector('input:checked').value;
     const computerChoice = getRandomThrow();
     const attempt = checkResult(userChoice, computerChoice);
@@ -83,7 +108,8 @@ const playARound = () => {
     }
     outcomeMessage.textContent = attempt;
     updateSpans();
-}
+    updateImages(userChoice, computerChoice);
+};
 
 // Event listeners
 resetButton.addEventListener('click', hardReset);
